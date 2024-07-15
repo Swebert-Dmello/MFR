5a87fe12ebf71698083072b96791b809261273fa
neenu89 <neenumath@gmail.com>
2024-07-15 21:13:04 +0530

    fixed some issue
    fixed some issue

---
+++ Scripts/AjaxCall.js
@@ -0,0 +1,30 @@
+﻿function callAjax(URL, type, parameter, funcSuccess, funcFailure) {
+    var d = new Date();
+    if (type.toLowerCase() == "get") {
+        $.ajax({
+            type: type,
+            url: URL + (URL.indexOf("?") > 0 ? '&d=' : '?d=') +
+                d.getDate() +
+                d.getHours() +
+                d.getMinutes() +
+                d.getMilliseconds(),
+            cache: false,
+            data: parameter,
+            async:true,
+            success: funcSuccess,
+            error: funcFailure
+        });
+    } else {
+        $.ajax({
+            type: type,
+            url: URL,
+            async: true,
+            cache: false,
+            data: parameter,
+            success: funcSuccess,
+            error: funcFailure
+        });
+
+    }
+}
+
